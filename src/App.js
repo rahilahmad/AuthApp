8a7d5a5068a60058f568e58987ff71850520a7d9
Raheel Ahmed <p136025@nu.edu.pk>
2018-10-17 13:00:01 +0500

    INITIAL COMMIT

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Facebook from './components/Facebook';
+
+class App extends Component {
+render(){
+  return(
+<div>
+ <p><Facebook /></p>
+</div>
+  );
+}
+
+
+
+
+}
+
+
+
+export default App;
+
+
+
