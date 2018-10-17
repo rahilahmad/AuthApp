55c28ef6782ebd37aaeafb674284a2ae7a52aeb5
Raheel Ahmed <p136025@nu.edu.pk>
2018-10-17 13:51:35 +0500

    google api added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Facebook from './components/Facebook';
+import Google from './components/Google';
 
 class App extends Component {
 render(){
   return(
 <div>
  <p><Facebook /></p>
+ <p><Google /></p>
 </div>
   );
 }
--- src/components/Google.js
+++ src/components/Google.js
@@ -1,8 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
 
 export default class Google extends Component {
-    
+  constructor(props){
+    super(props)
+    this.state={
+      isLoggedIn: true,
+    }
+  }
+    responseGoogle = (response) => {
+    console.log(response);
+  }
   render() {
+    let  gmContent;
+    if (this.state.isLoggedIn) {
+    }
+    else {
+      gmContent(
+        <GoogleLogin
+    clientId="408793953786-gb5hoipegnh5tqco7jn04u08a6ml2d5h.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={this.responseGoogle}
+    onFailure={this.responseGoogle}
+  />
+      )
+    }
     return (
       <div>
         
