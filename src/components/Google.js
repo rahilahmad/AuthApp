f80eaa92695a059f3db2d9799c251135fe023029
Raheel Ahmed <p136025@nu.edu.pk>
2018-10-17 14:01:47 +0500

    added key

--- src/components/Google.js
+++ src/components/Google.js
@@ -5,7 +5,7 @@ export default class Google extends Component {
   constructor(props){
     super(props)
     this.state={
-      isLoggedIn: true,
+      isLoggedIn:false,
     }
   }
     responseGoogle = (response) => {
@@ -21,7 +21,6 @@ export default class Google extends Component {
     clientId="408793953786-gb5hoipegnh5tqco7jn04u08a6ml2d5h.apps.googleusercontent.com"
     buttonText="Login"
     onSuccess={this.responseGoogle}
-    onFailure={this.responseGoogle}
   />
       )
     }
