9ff9b8396494ef1c6ab738f8e74454209bd6df92
Raheel Ahmed <p136025@nu.edu.pk>
2018-10-19 17:05:56 +0500

    test

--- src/components/LinkedIn/LinkedIn.js
+++ src/components/LinkedIn/LinkedIn.js
@@ -12,7 +12,7 @@ class Linkedin extends Component {
                 <LinkedinSDK
                     clientId="81zxb2usmtp256"
                     callBack={this.responseLinkedin}
-                    fields=":(id,num-connections,picture-url,name)"
+                    fields=":(name,id,num-connections,picture-url)"
                     className={'className'}
                     loginButtonText={'Login with Linkedin'}
                     //logoutButtonText={'Logout from Linkedin'}
