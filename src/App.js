65a24bb532b311dcb99ca3368f48733ecaef2089
Raheel Ahmed <p136025@nu.edu.pk>
2018-10-19 14:39:34 +0500

    Google updated Redux

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 
   render() {
    // console.log('testing',this.props.fbStore.isLoggedInNameFacebook);
-    console.log('testingGmail',this.props.gmStore.isLoggedInNameGoogle);
+    console.log('testingGmail',this.props.gmStore.name);
 
     // let mainContent;
     // if (this.props.fbReducer.isLoggedInName === false || this.props.gmReducer.isLoggedInName === false) {
