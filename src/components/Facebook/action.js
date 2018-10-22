ed614095452a5acb36147d0bc2772009d6fe9418
Raheel Ahmed <p136025@nu.edu.pk>
2018-10-22 12:45:34 +0500

    testing

--- src/components/Facebook/action.js
+++ src/components/Facebook/action.js
@@ -6,6 +6,12 @@ const isloggedInFacebook = (flag) => {
     }
 }
 const setFbLogin = (response) => {
+   if(repsonse.name === ''){
+       return (dispatch)=>{
+           dispatch(isloggedInFacebook(false));
+       }
+   }
+   else {
     return {
         type: 'RESPONSE_FACEBOOK',
         name: response.name,
@@ -13,6 +19,7 @@ const setFbLogin = (response) => {
         email: response.email,
         picture: response.picture.data.url
     }
+   }
 }
 
 export function repsonseFacebook(response) {
