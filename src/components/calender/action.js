ea87c6c42695df69a429ff0b86a8eb2a4624c053
Raheel Ahmed <p136025@nu.edu.pk>
2018-10-25 16:22:17 +0500

    testing

--- src/components/calender/action.js
+++ src/components/calender/action.js
@@ -4,28 +4,28 @@ const CALENDAR_ID = 'nu.edu.pk_rjvo0a8hogvtgcpavvol7u1sno@group.calendar.google.
 const API_KEY = 'AIzaSyBT4tlNuisZ1CO1SEAxkxsy2mBs6WW0bM8'
 let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`
 
- export function getEvents() {
+export function getEvents() {
     return (dispatch) => {
-       request
-        .get(url)
-        .end((err, resp) => {
-            if (!err) {
-                const events = []
-                JSON.parse(resp.text).items.map((event) => {
-                    events.push({
-                        start: event.start.date || event.start.dateTime,
-                        end: event.end.date || event.end.dateTime,
-                        title: event.summary,
+        request
+            .get(url)
+            .end((err, resp) => {
+                if (!err) {
+                    const events = []
+                    JSON.parse(resp.text).items.map((event) => {
+                        events.push({
+                            start: event.start.date || event.start.dateTime,
+                            end: event.end.date || event.end.dateTime,
+                            title: event.summary,
+                        })
                     })
-                })
-                dispatch(setCalenderEvents(events));
-                return events
-            }
-        })
-      }
+                    dispatch(setCalenderEvents(events));
+                    return events
+                }
+            })
+    }
 }
 
-const setCalenderEvents = (events)=>{
+const setCalenderEvents = (events) => {
     return {
         type: "GET_EVENTS",
         events,
--- src/components/calender/calender.js
+++ src/components/calender/calender.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { getEvents } from './action';
 import Calendar from "react-big-calendar";
 import moment from "moment";
-// import { getEvents } from './gcal';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import request from 'superagent';
 const CALENDAR_ID = 'nu.edu.pk_rjvo0a8hogvtgcpavvol7u1sno@group.calendar.google.com'
@@ -13,89 +12,87 @@ let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/event
 const localizer = Calendar.momentLocalizer(moment);
 
 class CalendarContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      events: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            events: []
+        }
     }
-  }
 
-  componentDidMount() {
-    this.props.getEvents();
-  }
-
-  handleSelect = ({ start, end }) => {
-    const title = window.prompt('New Event name')
-    if (title)
-      this.setState({
-        events: [
-          ...this.state.events,
-          {
-            start,
-            end,
-            title
-          },
-        ],
-      })
-    console.log(this.state.events)
-    this.postEvent
-  }
-  postEvent = () => {
-    const test = {
-      start: this.state.events.start,
-      end: this.state.events.end,
-      title: this.state.events.title
+    componentDidMount() {
+        this.props.getEvents();
     }
-    const test1 = JSON.stringify(test)
-    request
-      .post(url)
-      .send(test1)
-      .then((resp) => {
-        console.log('sasasa', resp)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-  render() {
-    console.log(this.state)
-    return (
-      <div>
-        <Calendar
-          popup
-          selectable
-          localizer={localizer}
-          culture='en-GB'
-          style={{ height: '50vh' }}
-          defaultView={Calendar.Views.MONTH}
-          events={this.state.events}
-          onSelectEvent={event => alert(event.title)}
-          onSelectSlot={this.handleSelect}
-        />
-        <a style={{
-          marginLeft: 800,
-        }}
-          className="primary-cta"
-          href="https://calendar.google.com/calendar?cid=c3FtMnVkaTFhZGY2ZHM3Z2o5aDgxdHVldDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
-          target="_blank"
-        >
-          <button>Create an Event</button>
-        </a>
-      </div>
 
-    )
-  }
+    handleSelect = ({ start, end }) => {
+        const title = window.prompt('New Event name')
+        if (title)
+            this.setState({
+                events: [
+                    ...this.state.events,
+                    {
+                        start,
+                        end,
+                        title
+                    },
+                ],
+            })
+        console.log(this.state.events)
+        this.postEvent
+    }
+    postEvent = () => {
+        const test = {
+            start: this.state.events.start,
+            end: this.state.events.end,
+            title: this.state.events.title
+        }
+        const test1 = JSON.stringify(test)
+        request
+            .post(url)
+            .send(test1)
+            .then((resp) => {
+                console.log('sasasa', resp)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+    render() {
+        return (
+            <div>
+                <Calendar
+                    popup
+                    selectable
+                    localizer={localizer}
+                    culture='en-GB'
+                    style={{ height: '50vh' }}
+                    defaultView={Calendar.Views.MONTH}
+                    events={this.props.events || []}
+                    onSelectEvent={event => alert(event.title)}
+                    onSelectSlot={this.handleSelect}
+                />
+                <a style={{
+                    marginLeft: 800,
+                }}
+                    className="primary-cta"
+                    href="https://calendar.google.com/calendar?cid=c3FtMnVkaTFhZGY2ZHM3Z2o5aDgxdHVldDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
+                    target="_blank"
+                >
+                    <button>Create an Event</button>
+                </a>
+            </div>
+
+        )
+    }
 }
 
-const mapDispatchToProps = dispatch => ({
-    getEvents: dispatch(getEvents),
-});
+const mapDispatchToProps = {
+    getEvents
+}
 
 const mapStateToProps = (state) => {
-    console.log("/////////////", state)
     return {
-        events: state.events
+        events: state.calender.events
     }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(CalendarContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(CalendarContainer);
