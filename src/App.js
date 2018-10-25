6494b5acba92c0a3c96efd18c39d73a50f3eb8c7
Raheel Ahmed <p136025@nu.edu.pk>
2018-10-25 16:55:36 +0500

    testing

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
     else {
       mainContent = (
         <div>
-          <p style={{margin= 100}}><Linkedin /></p>
+          <p style={{ margin: '50vh' }}><Linkedin /></p>
           <CalendarContainer />
         </div>
       )
